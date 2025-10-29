cf2b9f2cceee6f0aa29b98d399cc5806b26c8564
tomus25 <tomus25@icloud.com>
2025-10-28 18:09:45 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, experimental: { appDir: true } };
+const nextConfig = {
+  reactStrictMode: true,
+};
+
 module.exports = nextConfig;
